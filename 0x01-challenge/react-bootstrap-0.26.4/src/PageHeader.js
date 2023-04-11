32a0c5e10ab3216d15881f782b31ca41bf800917
Maximus <90454403+maxi-musz@users.noreply.github.com>
2023-04-10 21:34:59 -0700

    Add files via upload

--- 0x01-challenge/react-bootstrap-0.26.4/src/PageHeader.js
+++ 0x01-challenge/react-bootstrap-0.26.4/src/PageHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import classNames from 'classnames';
 
 const PageHeader = React.createClass({
   render() {
     return (
-      <div {...this.props} className={classNames(this.props.className, 'page-header')}>
-        <h1>{this.props.children}</h1>
+      <div className="bs-docs-header" id="content">
+        <div className="container">
+          <h1>{this.props.title}</h1>
+          <p>{this.props.subTitle}</p>
+        </div>
       </div>
     );
   }
